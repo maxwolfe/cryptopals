a8312e76a79006689cf3445333ca3561168543b6
Max Wolfe <max@securitywolfe.com>
2020-05-22 17:33:06 -0700

    Updated dictionary solving

--- cryptopals/solvers.py
+++ cryptopals/solvers.py
@@ -1,50 +1,36 @@
 from cryptopals.conversions import (
     hex_to_ascii,
 )
-from textstat import (
-        flesch_reading_ease as fre_score,
-        smog_index as si_score,
-        flesch_kincaid_grade as fkg_score,
-        coleman_liau_index as cli_score,
-        automated_readability_index as ari_score,
-        dale_chall_readability_score as dcr_score,
-        difficult_words as dw_score,
-        linsear_write_formula as lwf_score,
-        gunning_fog as gf_score,
-        text_standard as ts_score,
+from enchant import (
+        Dict,
 )
+
+dictionary = Dict('en_US')
 
 
 def score_string(string):
     '''
-    Score a string for readability
+    Score a string for english words
 
     :param string: input string to score
-    :return: an arbitrary score for readability
+    :return: an arbitrary score for number of english words
     '''
 
-    '''
-    valuable_indexes = [
-            fre_score,
-            si_score,
-            fkg_score,
-            cli_score,
-            ari_score,
-            dcr_score,
-            dw_score,
-            lwf_score,
-            gf_score,
-            ts_score
-    ]
-    '''
+    words = []
 
-    # Sanitize non-strings
-    for char in string:
-        if ord(char) < 0x20 or ord(char) > 0x7e:
-            return 0
+    # Remove words with invalid characters before check
+    for word in string.split(' '):
+        for c in word:
+            if c < 'A' or (c > 'Z' and c < 'a') or c > 'z':
+                break
+        else:
+            if word:
+                words.append(word)
 
-    # Use only fre score for now
-    return fre_score(string)
+    return sum(map(
+        lambda x: 1 if dictionary.check(x) else 0,
+        words,
+    ))
 
 
 def find_best_plaintext(candidates):
@@ -56,14 +42,15 @@ def find_best_plaintext(candidates):
     '''
 
     # Find score for all plaintexts (might need multiple later)
-    scores = (score_string(candidate) for candidate in candidates)
+    scored_candidate = (
+            (score_string(candidate), candidate) for candidate in candidates
+    )
 
     # Return the most highly scored
     return sorted(
-            zip(candidates, scores),
-            key=lambda x: x[1],
+            scored_candidate,
             reverse=True,
-    )[0][0]
+    )[0][1]
 
 
 def solve_single_byte_xor(hex_ciphertext):
@@ -80,9 +67,26 @@ def solve_single_byte_xor(hex_ciphertext):
             ret_bytes=True,
     )
     # Generate a list of all possible plaintexts
-    candidates = list(map(
+    candidates = map(
             lambda x: "".join([chr(char ^ x) for char in initial_str]),
             range(256),
-    ))
+    )
+
+    return find_best_plaintext(candidates)
+
+
+def detect_single_byte_xor(list_of_ciphertexts):
+    '''
+    Detect which ciphertext is single-byte encrypted
+
+    :param list_of_ciphertexts: a list of possible encrypted strings
+    :return: most likely result
+    '''
+
+    # Find the single_byte_xor solution for each string
+    candidates = map(
+            solve_single_byte_xor,
+            list_of_ciphertexts,
+    )
 
     return find_best_plaintext(candidates)
